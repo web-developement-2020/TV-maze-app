e14ccd4cb3154164982cac1fb30914d35c9e89ae
KLishi <lishitha92@gmail.com>
2020-08-12 11:53:23 -0700

    new component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DisplayPageComponent } from './display-page/display-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisplayPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/display-page/display-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-display-page',
+  templateUrl: './display-page.component.html',
+  styleUrls: ['./display-page.component.css']
+})
+export class DisplayPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
